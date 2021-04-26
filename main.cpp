4fd2d225b9ac8904a132df36a56a9bb78bf081d0
Pratik Bhattacharjee <pratikbhattacharjee09@gmail.com>
2021-04-26 18:08:02 +0800

    Added weapon functions and welcome message
    Added functions
    Welcome message - Reads from file and prints welcome message
    
    FetchItem - Searches for an item in the galaxy
    createweapon - Creates weapon

--- main.cpp
+++ main.cpp
@@ -25,3 +25,55 @@ struct Position{//Position struct
   int row;
   int col;
 };
+
+void welcomeMessage(){
+  //Welcome message;
+  string line = "";//for reading each lin
+  ifstream fin;
+  fin.open("welcome.txt");//opening the file for reading
+  while(getline(fin,line)){
+    cout<<line<<endl;
+  }
+  fin.close();//
+}
+
+//function to search items in in map
+void fetchitem(int map[][8], Position Pos, Position &findPos, int val){
+  for(int row = Pos.row -1;row <= Pos.row +1;row++){//scanning rows
+    if(row < 0 || row>7){//if out of map range
+      continue;
+    }
+    for(int col = Pos.col-1;col<= Pos.col+1; col++){//scanning columns
+      if(col<0 || col>7){//if out of map range
+        continue;
+      }
+      if(col == Pos.col && row == Pos.row)//avoid player position
+        continue;
+
+      if((row == Pos.row || col == Pos.col)){
+          if (map[row][col] == val){//item found
+            findPos.row = row;
+            findPos.col = col;
+          }
+    }
+    }
+  }
+}
+
+//function to create weapons
+void createweapon(int val){
+  Weapon n;//create new weapon
+  if(val == 0 )
+    n.type = "GAS MASK";
+  if(val == 1 )
+    n.type = "LIGHT SABER";
+  if(val == 2 )
+    n.type = "PORTAL GUN";
+  n.password = createPass();//Generate random password
+  weapons.push_back(n);//add weapon to vector
+  //Weapon information to player
+  cout<<"****************"<<endl;
+  cout<<"YOU GOT: "<<n.type<<endl;
+  cout<<"PassWord: "<<n.password<<endl;
+  cout<<"****************"<<endl;
+}
